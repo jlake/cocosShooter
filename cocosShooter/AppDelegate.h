c2fae6aa79b90270c587b662c1fbb22222edfd25
Jlake Ou <ouzhiwei@gmail.com>
2011-05-09 18:03:24 +0900

    Initial Commit

---
+++ cocosShooter/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  cocosShooter
+//
+//  Created by 欧 on 11/05/09.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ cocosShooter/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  cocosShooter
+//
+//  Created by 欧 on 11/05/09.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
